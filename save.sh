1823d308acb0b8c80e8abcd1c88b4a49cbf2c5c9
44Schwarz <sirmitya@gmail.com>
2019-12-10 21:40:38 +0300

    Initial commit

---
+++ save.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# A script to save web pages to the web archive
+# Pass a filename with urls or a single url
+
+BASE_URL='https://web.archive.org/save/'
+
+usage() {
+	echo "Usage: ./save.sh [[-f file] | [-u url] | [-h]]"
+}
+
+make_request() {
+	if [ "$1" == "" ]; then
+		return;
+	fi
+	full_url="$BASE_URL$1"
+	# echo $full_url
+	curl --silent -I $full_url 1>> work.log
+	if [ $? != 0 ]; then
+		echo "Something went wrong while fetching $1"
+	fi
+}
+
+read_file() {
+	file=$1
+	if [ -f $file ]; then
+		while IFS='' read -r line; do
+			make_request $line
+		done < $file
+	else
+		echo "File $file does not exist"
+		exit 2
+	fi
+
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -f | --file )           shift
+                                read_file $1
+                                ;;
+        -u | --url )            shift
+		                make_request $1
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
